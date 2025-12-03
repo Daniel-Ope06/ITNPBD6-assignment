c516bfe3800a7b64784ff4e4e34d9f1943d0dabc
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2025-12-03 15:22:27 +0000

    chore: Go through each categorical column

--- experiments/01_eda.py
+++ experiments/01_eda.py
@@ -19,3 +19,24 @@ from src.data_cleaning import (
 wallace = load_wallace_data()
 wallace.head()
 wallace.info()
+
+# Viewing categorical columns
+wallace["town"].value_counts()
+wallace["country"].value_counts()
+wallace["job"].value_counts()
+wallace["married"].value_counts()
+wallace["education"].value_counts()
+wallace["arrears"].value_counts()
+wallace["housing"].value_counts()
+
+# Some have 'n' instead of 'no' in 'has_tv_package'
+wallace["has_tv_package"].value_counts()
+
+# Some have 'cell' instead of 'cellular' in 'last_contact'
+wallace["last_contact"].value_counts()
+
+# One record has 'j' instead of an actual month
+wallace["last_contact_this_campaign_month"].value_counts()
+
+wallace["outcome_previous_campaign"].value_counts()
+wallace["new_contract_this_campaign"].value_counts()
