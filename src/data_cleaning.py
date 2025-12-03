f7c3e3659a23f24718aebf4f4f9cbfb6d20f9cd9
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2025-12-03 15:56:49 +0000

    feat: Define `clean_wallace_data()` to fix typos and inconsistencies in categorical columns

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -9,3 +9,25 @@ WALLACE_PATH = os.path.join(
 def load_wallace_data(filepath: str = WALLACE_PATH) -> pd.DataFrame:
     """Loads raw data from a CSV file."""
     return pd.read_csv(filepath)
+
+
+def clean_wallace_data(data: pd.DataFrame) -> pd.DataFrame:
+    """Fix typos and inconsistencies."""
+    clean_data: pd.DataFrame = data.copy()
+
+    # Fix 'n' -> 'no' in 'has_tv_package'
+    clean_data['has_tv_package'] = clean_data['has_tv_package'].replace(
+        {'n': 'no'}
+    )
+
+    # Fix 'cell' -> 'cellular' in 'last_contact'
+    clean_data['last_contact'] = clean_data['last_contact'].replace(
+        {'cell': 'cellular'}
+    )
+
+    # Drop the single row with typo 'j' in month
+    clean_data = clean_data[
+        clean_data['last_contact_this_campaign_month'] != 'j'
+    ]
+
+    return clean_data
