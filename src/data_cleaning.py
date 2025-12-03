615af95288573ef3eb34b13e1ce118cfd7613493
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2025-12-03 16:58:12 +0000

    feat: Update `clean_wallace_data()` based on histogram observations

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from pandas import Series
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 WALLACE_PATH = os.path.join(
@@ -15,6 +16,7 @@ def clean_wallace_data(data: pd.DataFrame) -> pd.DataFrame:
     """Fix typos and inconsistencies."""
     clean_data: pd.DataFrame = data.copy()
 
+    # -- Fix typos --
     # Fix 'n' -> 'no' in 'has_tv_package'
     clean_data['has_tv_package'] = clean_data['has_tv_package'].replace(
         {'n': 'no'}
@@ -29,5 +31,20 @@ def clean_wallace_data(data: pd.DataFrame) -> pd.DataFrame:
     clean_data = clean_data[
         clean_data['last_contact_this_campaign_month'] != 'j'
     ]
+
+    # -- Handle 'days_since_last_contact' (-1 handling) --
+    # Create new binary column True(1), False(0)
+    clean_data['never_contacted'] = (
+        clean_data['days_since_last_contact_previous_campaign'] == -1
+    ).astype(int)
+
+    # Replace -1 with 2x Max to push it to the far past
+    contacted_mask: Series[bool] = clean_data[
+        'days_since_last_contact_previous_campaign'] != -1
+    max_days: int = clean_data.loc[
+        contacted_mask, 'days_since_last_contact_previous_campaign'].max()
+    fill_value: int = 2 * max_days
+    clean_data['days_since_last_contact_previous_campaign'] = clean_data[
+        'days_since_last_contact_previous_campaign'].replace(-1, fill_value)
 
     return clean_data
